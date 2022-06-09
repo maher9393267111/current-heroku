d858120abe35749cb4c965da463e9ba672de44ea
maher <gomemahero.com>
2022-06-09 21:26:54 +0300

    recive single image from clouduinary

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,103 +3,65 @@ import { useDropzone } from "react-dropzone";
 import axios from "axios";
 
 const Home = () => {
-  const [uploadClass, setUploadClass] = useState("class-images");
-  const [images, setImages] = useState([]);
+ 
+  const [image, setImage] = useState('');
   const [imageurl, setImageurl] = useState([]);
-  const [data, setData] = useState('');
-
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: "image/*",
-    maxSize: 40000000000000000000000000,
-    maxFiles: 50,
-    multiple: true,
-    onDrop: (acceptedFiles) => {
-      acceptedFiles.forEach((file) => {
-        const reader = new FileReader();
-
-        reader.onabort = () => console.log("file reading was aborted");
-        reader.onerror = () => console.log("file reading has failed");
-        reader.onload = () => {
-          // Do whatever you want with the file contents
-          const imgUrl = reader.result;
-          setImages((s) => [imgUrl, ...s]);
-          console.log(images);
-        };
-        reader.readAsDataURL(file);
-       // setImageurl((s) => [file, ...s]);
-        // console.log(imageurl);
-      });
-      setUploadClass("class-images");
-    },
-    onDragEnter: () => {
-      setUploadClass("class-images-uploading");
-    },
-    onDragLeave: () => {
-      setUploadClass("class-images");
-    },
-  });
+  const [data, setData] = useState("");
 
 
 
-// send images to server upolad rouetr
+const handleimagechange = (e) => {
 
-const upload = async (req, res) => {
-    try {
-       const response = await axios.post('http://localhost:5000/api/upload', {
-        images: images
-        });
-        console.log(response.data);
-        setData(response.data.images);   
-        console.log(data);
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    reader.onloadend = () => {
+// resize image  to fit in the image box
+        
+       // img.src = reader.result;
+      
+
+      setImage(reader.result);
+      console.log(reader.result);
     }
-    catch(err){
-        console.log(err);
-    }
+    reader.readAsDataURL(file);
+  
+  
+
 }
 
 
 
 
+  // send images to server upolad rouetr
+
+  const upload = async (req, res) => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/upload", {
+        image: image,
+      });
+      console.log(response.data);
+      setData(response.data.images);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div className="class">
-      <div
-        {...getRootProps()}
-        className={uploadClass}
-        style={{ background: images.length === 0 ? "none" : "white" }}
-      >
-        <input {...getInputProps()} />
-        {images.length === 0 ? (
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        ) : (
-          <div
-            className="  ml-20  mt-12"
-            style={{ display: "flex", gap: "10px" }}
-          >
-            {images.map((img, i) => (
-              <div className="img-div" key={i}>
-                <img style={{ width: "100px", height: "80px" }} src={img}></img>
-              </div>
-            ))}
-          </div>
-        )}
+    <div className="  home-contaioner">
+
+<input
+onChange={handleimagechange}
+
+type="file" multiple='false' />
+
+
+<button
+onClick={upload}
+>send  to server</button>
+
+
       </div>
-      <button className="img-delete-button" onClick={() => setImages([])}>
-        X
-      </button>
-
-        <button
-        onClick={upload}
-
-        > send</button>
-
-        <div>
-
-<img src={data[0]} alt="" />
-
-
-        </div>
-    </div>
   );
 };
 
